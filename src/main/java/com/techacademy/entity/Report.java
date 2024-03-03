06c898da9e9d1fd7006b24102322c9f40a46f02f
tasayak <sayaka.t.0907@gmail.com>
2024-03-03 14:36:10 +0900

    日報一覧画面（名前表示、権限による表示振り分け以外）

---
+++ src/main/java/com/techacademy/entity/Report.java
@@ -0,0 +1,67 @@
+
+package com.techacademy.entity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.SQLRestriction;
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "reports")
+@SQLRestriction("delete_flg = false")
+public class Report {
+
+    @Id
+    @Column(length = 10)
+    @NotEmpty
+    @Length(max = 10)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    // 日付
+    @Column(nullable = false)
+    private LocalDate reportDate;
+
+    // タイトル
+    @Column(length = 100, nullable = false)
+    @NotEmpty
+    @Length(max = 100)
+    private String title;
+
+    // 内容
+    @Column(columnDefinition="LONGTEXT", nullable = false)
+    @NotEmpty
+    @Length(max = 600)
+    private String content;
+
+    // 削除フラグ(論理削除を行うため)
+    @Column(columnDefinition="TINYINT", nullable = false)
+    private boolean deleteFlg;
+
+    // 登録日時
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    // 更新日時
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_code", referencedColumnName = "code", nullable = false)
+    private Employee employee;
+
+
+}
---
+++ src/main/java/com/techacademy/service/ReportService.java
@@ -0,0 +1,45 @@
+package com.techacademy.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.techacademy.constants.ErrorKinds;
+import com.techacademy.entity.Report;
+import com.techacademy.repository.ReportRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ReportService {
+
+    private final ReportRepository reportRepository;
+
+    @Autowired
+    public ReportService(ReportRepository reportRepository) {
+        this.reportRepository = reportRepository;
+    }
+
+
+
+    // 日報一覧表示処理
+    public List<Report> findAll() {
+        return reportRepository.findAll();
+    }
+
+    // 1件を検索
+    public Report findByCode(String id) {
+        // findByIdで検索
+        Optional<Report> option = reportRepository.findById(id);
+        // 取得できなかった場合はnullを返す
+        Report report = option.orElse(null);
+        return report;
+    }
+}
+
+
